bac274b051db34bd3017ebb5dce6fe9474a4a980
giorgio <giorgiosavron@me.com>
2023-05-27 08:46:14 -0400

    css placement

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,36 +6,36 @@ import github from "../images/github.png";
 export const Footer = () => {
   return (
     <>
-      <div className="position-absolute bottom-0 start-0 footer footer-left">
-        &copy; Giorgio Savron
-      </div>
-      <div className="position-absolute bottom-0 end-0 footer footer-right">
-        <div>
-          <a
-            href="https://www.giorgiosavron.com"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={gs_logo} alt="GS_logo" className="ficon" />
-          </a>
-        </div>
-        <div>
-          <a
-            href="https://www.linkedin.com/in/giorgio-savron/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={linkedin} alt="Linkedin" className="ficon" />
-          </a>
-        </div>
-        <div>
-          <a
-            href="https://github.com/gsavr/g2i-React"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={github} alt="github" className="ficon" />
-          </a>
+      <div className="fixed-bottom d-flex flex-row w-100 justify-content-between align-items-end">
+        <div className=" footer footer-left">&copy; Giorgio Savron</div>
+        <div className=" footer footer-right">
+          <div>
+            <a
+              href="https://www.giorgiosavron.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={gs_logo} alt="GS_logo" className="ficon" />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://www.linkedin.com/in/giorgio-savron/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={linkedin} alt="Linkedin" className="ficon" />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://github.com/gsavr/g2i-React"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={github} alt="github" className="ficon" />
+            </a>
+          </div>
         </div>
       </div>
     </>
